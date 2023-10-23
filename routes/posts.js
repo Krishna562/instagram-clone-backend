081c87c419fa055a9ee35affaf1ce99e2caa598f
Robin562i <robin2007562k@gmail.com>
2023-10-23 15:58:33 +0530

    added delete post route and controller

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createPost, likePost, addComment } from "../controllers/posts.js";
+import {
+  createPost,
+  likePost,
+  addComment,
+  deleteComment,
+} from "../controllers/posts.js";
 import isValidUser from "../utils/isValidUser.js";
 
 const postsRouter = express.Router();
@@ -9,5 +14,7 @@ postsRouter.put("/createPost", isValidUser, createPost);
 postsRouter.patch("/like-post", isValidUser, likePost);
 
 postsRouter.patch("/add-comment", isValidUser, addComment);
+
+postsRouter.patch("/delete-comment", isValidUser, deleteComment);
 
 export default postsRouter;
